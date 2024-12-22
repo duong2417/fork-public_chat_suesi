3593cd305600cd97a1eb5a092418a54a6c557a47
duong2417 <ngocduong1724@gmail.com>
2024-12-22 14:39:17 +0700

    thêm dragable only  bubble

--- lib/features/chat_with_gemini/ui/chat_with_gemini_bubble.dart
+++ lib/features/chat_with_gemini/ui/chat_with_gemini_bubble.dart
@@ -11,13 +11,13 @@ class ChatWithGeminiBubble extends StatefulWidget {
 class _ChatWithGeminiBubbleState extends State<ChatWithGeminiBubble> {
   bool _isChatOpen = false;
   bool _isFullScreen = false;
-  Offset _bubblePosition = Offset(20, 50);
+  Offset _bubblePosition = const Offset(20, 50);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // Bong bóng chat và Hộp chat
+        // Bong bóng chat
         Positioned(
           bottom: _bubblePosition.dy,
           right: _bubblePosition.dx,
@@ -35,54 +35,53 @@ class _ChatWithGeminiBubbleState extends State<ChatWithGeminiBubble> {
                 _isChatOpen = !_isChatOpen;
               });
             },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.blue,
-                  child: Icon(Icons.message, color: Colors.white),
-                ),
-                if (_isChatOpen)
-                  Container(
-                    width: _isFullScreen
-                        ? MediaQuery.of(context).size.width
-                        : MediaQuery.of(context).size.width * 0.7,
-                    height: _isFullScreen
-                        ? MediaQuery.of(context).size.height
-                        : 300,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: _isFullScreen
-                          ? BorderRadius.zero
-                          : BorderRadius.circular(15),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 10,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: GenaiSettingScreen(
-                      isFullScreen: _isFullScreen,
-                      onTapFullScreen: () {
-                        setState(() {
-                          _isFullScreen = !_isFullScreen;
-                        });
-                      },
-                      onTapClose: () {
-                        setState(() {
-                          _isChatOpen = false;
-                          _isFullScreen = false;
-                        });
-                      },
-                    ),
-                  ),
-              ],
+            child: const CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.blue,
+              child: Icon(Icons.message, color: Colors.white),
             ),
           ),
         ),
+
+        // Hộp chat
+        if (_isChatOpen)
+          Positioned(
+            bottom: _isFullScreen ? 0 : 110,
+            right: _isFullScreen ? 0 : 20,
+            child: Container(
+              width: _isFullScreen
+                  ? MediaQuery.of(context).size.width
+                  : MediaQuery.of(context).size.width * 0.7,
+              height: _isFullScreen ? MediaQuery.of(context).size.height : 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: _isFullScreen
+                    ? BorderRadius.zero
+                    : BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: GenaiSettingScreen(
+                isFullScreen: _isFullScreen,
+                onTapFullScreen: () {
+                  setState(() {
+                    _isFullScreen = !_isFullScreen;
+                  });
+                },
+                onTapClose: () {
+                  setState(() {
+                    _isChatOpen = false;
+                    _isFullScreen = false;
+                  });
+                },
+              ),
+            ),
+          ),
       ],
     );
   }
